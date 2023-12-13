77b083420d8253f5422f7f1fc0639243380865c9
Michael Averto <mjaverto@channelape.com>
2023-12-13 11:13:34 -0500

    Add support for multipart

--- app.js
+++ app.js
@@ -1,69 +1,98 @@
-const express = require("express");
-const cors = require("cors");
-const axios = require("axios");
-const querystring = require("querystring");
-const winston = require("winston");
+const express = require('express');
+const multer = require('multer');
+const querystring = require('querystring');
+const axios = require('axios');
+const winston = require('winston');
+const cors = require('cors');
 
+
+// Initialize Express app
 const app = express();
 
-app.use(cors());
+
+app.use(cors())
+
 
 // Winston logger setup
 const logger = winston.createLogger({
-  level: "info",
+  level: 'info',
   format: winston.format.combine(
     winston.format.timestamp(),
-    winston.format.json() // Ensures single-line JSON output
+    winston.format.printf(info => JSON.stringify({ 
+      level: info.level, 
+      message: info.message, 
+      timestamp: info.timestamp 
+    }))
   ),
   transports: [
-    new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.json()
-      ),
-    }),
-  ],
+    new winston.transports.Console()
+  ]
 });
 
-// Use environment variable for baseUrl, with a default fallback
-const baseUrl = process.env.BASE_URL || "https://browser-intake-datadoghq.com";
 
-app.get("/health", (_, res) => res.sendStatus(200));
 
+// Configure Multer (adjust storage and limits as needed)
+const upload = multer();
+
+app.get('/health', (_, res) => res.sendStatus(200));
+
+// The main POST route
 app.post('/', (req, res) => {
-  let data = '';
-  req.on('data', chunk => {
-    data += chunk;
-  });
-  req.on('end', async () => {
-    // Construct the full URL
-    const forwardPath = decodeURIComponent(req.query.ddforward);
-    const queryParams = {...req.query};
-    delete queryParams.ddforward; // Remove the ddforward parameter.
+  const contentType = req.headers['content-type'];
+  logger.info(`Received content type: ${contentType}`);
 
-    const fullUrl = `${baseUrl}${forwardPath}?${querystring.stringify(queryParams)}`;
-    logger.info(`Forwarding to URL: ${fullUrl}`);
-    logger.info(`Body: ${data}`);
-
-    try {
-      const response = await axios.post(
-        fullUrl,
-        data, // Use the raw data as the body
-        {
-          headers: {
-            'Content-Type': 'text/plain;charset=UTF-8',
-            // Include other necessary headers
-          },
-        }
-      );
-      logger.info('Response data:', { data: response.data });
-      res.status(response.status).send(response.data);
-    } catch (error) {
-      logger.error('Error occurred', { error: error.message, stack: error.stack });
-      res.status(error.response?.status || 500).send(error.response?.data || 'Internal Server Error');
-    }
-  });
+  if (contentType && contentType.includes('multipart/form-data')) {
+    logger.info('Handling multipart/form-data');
+    upload.any()(req, res, (err) => {
+      if (err) {
+        logger.error('Multer error:', err);
+        return res.status(500).send('Error processing multipart/form-data');
+      }
+      // Continue processing the request with multer-parsed data
+      handleRequest(req, res);
+    });
+  } else {
+    // Handle other content types (like text/plain)
+    let data = '';
+    req.on('data', chunk => {
+      data += chunk;
+    });
+    req.on('end', () => {
+      req.body = data; // Set the raw data to req.body
+      handleRequest(req, res);
+    });
+  }
 });
 
+// Com`mo`n request handling logic
+function handleRequest(req, res) {
+  const forwardPath = decodeURIComponent(req.query.ddforward);
+  const queryParams = {...req.query};
+  delete queryParams.ddforward;
+  const baseUrl = process.env.BASE_URL || "https://browser-intake-datadoghq.com";
+
+
+  const fullUrl = `${baseUrl}${forwardPath}?${querystring.stringify(queryParams)}`;
+  logger.info(`Forwarding to URL: ${fullUrl}`);
+  
+  if (req.files) {
+    logger.info(`Files: ${req.files.map(file => file.originalname).join(", ")}`);
+  }else{
+    logger.info(`Body: ${req.body}`);
+  }
+
+  axios.post(fullUrl, req.body, {
+    headers: {
+      'Content-Type': req.headers['content-type'], // Forward the original Content-Type
+      'X-Forwarded-For': req.headers['x-forwarded-for'],
+    },
+  }).then(response => {
+    logger.info('Response data:', { data: response.data });
+    res.status(response.status).send(response.data);
+  }).catch(error => {
+    logger.error('Error occurred', { error: error.message, stack: error.stack });
+    res.status(error.response?.status || 500).send(error.response?.data || 'Internal Server Error');
+  });
+}
 
 module.exports = app;
