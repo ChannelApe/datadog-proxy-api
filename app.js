5cad12fcae1b5cd3cb7bc5baa367b95e36b215c4
Mike Averto <603286+mjaverto@users.noreply.github.com>
2023-12-13 08:03:18 -0500

    Update app.js
    Update for correct URL parsing and add baseUrl envvar with default being US

--- app.js
+++ app.js
@@ -1,32 +1,50 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require("axios");
+const querystring = require('querystring');
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
+app.use(express.json());
+
+// Use environment variable for baseUrl, with a default fallback
+const baseUrl = process.env.BASE_URL || 'https://browser-intake-datadoghq.com';
 
 app.get('/health', (_, res) => res.sendStatus(200));
 
 app.post('/', async (req, res) => {
-  await axios.post(
-    req.query.ddforward,
-    req.body,
-    {
-      headers: {
-        Accept: req.headers.accept,
-        'Content-Type': req.headers['content-type'],
-        'X-Forwarded-For': req.headers['x-forwarded-for'],
-      },
-    }
-  ).then(function (response) {
-    console.log(response?.data);
-  }).catch(function (error) {
-    console.error(error);
-    console.error(JSON.stringify(error.response?.data));
-  });
+  const forwardPath = decodeURIComponent(req.query.ddforward);
 
-  return res.sendStatus(200);
+  if (!forwardPath) {
+    console.error('No ddforward parameter provided or it is invalid');
+    return res.status(400).send('No ddforward parameter provided or it is invalid');
+  }
+
+  const queryParams = {...req.query};
+  delete queryParams.ddforward; // Remove the ddforward parameter.
+
+  const fullUrl = `${baseUrl}${forwardPath}?${querystring.stringify(queryParams)}`;
+  console.log('Forwarding to URL:', fullUrl); // Log the URL for debugging
+
+  try {
+    const response = await axios.post(
+      fullUrl,
+      req.body,
+      {
+        headers: {
+          Accept: req.headers.accept,
+          'Content-Type': req.headers['content-type'],
+          'X-Forwarded-For': req.headers['x-forwarded-for'],
+        },
+      }
+    );
+    console.log('Response data:', response.data);
+    res.status(response.status).send(response.data);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(error.response?.status || 500).send(error.response?.data || 'Internal Server Error');
+  }
 });
 
 module.exports = app;
