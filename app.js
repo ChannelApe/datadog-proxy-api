44f5454b259687b73cac597e93e6d81b7552e474
Michael Averto <mjaverto@channelape.com>
2023-12-13 11:57:08 -0500

    Simplify, just stream

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const querystring = require('querystring');
 const axios = require('axios');
 const winston = require('winston');
 const cors = require('cors');
-
+const FormData = require('form-data');
 
 // Initialize Express app
 const app = express();
+
+// Define the hostname
+const ddhostname = process.env.ddhostname || "browser-intake-datadoghq.com";
 
 
 app.use(cors())
@@ -38,61 +41,34 @@ app.get('/health', (_, res) => res.sendStatus(200));
 
 // The main POST route
 app.post('/', (req, res) => {
-  const contentType = req.headers['content-type'];
-  logger.info(`Received content type: ${contentType}`);
-
-  if (contentType && contentType.includes('multipart/form-data')) {
-    logger.info('Handling multipart/form-data');
-    upload.any()(req, res, (err) => {
-      if (err) {
-        logger.error('Multer error:', err);
-        return res.status(500).send('Error processing multipart/form-data');
-      }
-      // Continue processing the request with multer-parsed data
-      handleRequest(req, res);
-    });
-  } else {
-    // Handle other content types (like text/plain)
-    let data = '';
-    req.on('data', chunk => {
-      data += chunk;
-    });
-    req.on('end', () => {
-      req.body = data; // Set the raw data to req.body
-      handleRequest(req, res);
-    });
-  }
-});
-
-// Com`mo`n request handling logic
-function handleRequest(req, res) {
   const forwardPath = decodeURIComponent(req.query.ddforward);
   const queryParams = {...req.query};
   delete queryParams.ddforward;
-  const baseUrl = process.env.BASE_URL || "https://browser-intake-datadoghq.com";
-
+  const baseUrl = `https://${ddhostname}`;
 
   const fullUrl = `${baseUrl}${forwardPath}?${querystring.stringify(queryParams)}`;
-  logger.info(`Forwarding to URL: ${fullUrl}`);
-  
-  if (req.files) {
-    logger.info(`Files: ${req.files.map(file => file.originalname).join(", ")}`);
-  }else{
-    logger.info(`Body: ${req.body}`);
-  }
 
-  axios.post(fullUrl, req.body, {
+  axios.post(fullUrl, req, {
     headers: {
-      'Content-Type': req.headers['content-type'], // Forward the original Content-Type
-      'X-Forwarded-For': req.headers['x-forwarded-for'],
+      ...req.headers,
+      'host': ddhostname,
     },
-  }).then(response => {
-    logger.info('Response data:', { data: response.data });
+  })
+  .then(response => {
     res.status(response.status).send(response.data);
-  }).catch(error => {
-    logger.error('Error occurred', { error: error.message, stack: error.stack });
+  })
+  .catch(error => {
+    const errorDetails = {
+      message: error.message,
+      status: error.response?.status,
+      statusText: error.response?.statusText,
+      responseBody: error.response?.data
+    };
+    logger.error('Error occurred', errorDetails);
+  
     res.status(error.response?.status || 500).send(error.response?.data || 'Internal Server Error');
   });
-}
+});
+
 
 module.exports = app;
